d31e896c4688d17e4a0f7cfb2629ea2fe66c8183
shivamg829 <shivamgangwar2197@gmail.com>
2026-08-08 12:55:30 +0530

    File I/O

---
+++ Chapter 8/file.py
@@ -0,0 +1,11 @@
+# Read file
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
+
+# Write to file
+f = open("filew.txt", "w")
+f.write("This is a new line of text.\n")
+f.write("This is another line of text.\n")
+f.close()

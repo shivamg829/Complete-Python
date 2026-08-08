3fa6db980b5811ba3bb66cbb89a850d8273eab9b
shivamg829 <shivamgangwar2197@gmail.com>
2026-08-08 12:58:04 +0530

    File I/O

--- Chapter 8/file.py
+++ Chapter 8/file.py
@@ -9,3 +9,8 @@ f = open("filew.txt", "w")
 f.write("This is a new line of text.\n")
 f.write("This is another line of text.\n")
 f.close()
+
+# with statement
+with open("file.txt") as f:
+    data = f.read()
+    print(data)

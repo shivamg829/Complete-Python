a23cc574a44ef43b99f80ff8247df8d54ab01a67
shivamg829 <shivamgangwar2197@gmail.com>
2026-08-08 05:54:03 +0530

    Print, Module & Comment

---
+++ Chapter 1/module.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+# Module: Chapter 1
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)
+
+# Comment: This code prints "Hello, World!" and then fetches a random joke using the pyjokes library and prints it.
